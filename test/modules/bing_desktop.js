181f2dc851b5350791280b1762544968718132da
niteshJhaKr <nitesh.jha@collegedunia.com>
2021-09-15 18:05:40 +0530

    add test case

--- test/modules/bing_desktop.js
+++ test/modules/bing_desktop.js
@@ -1,57 +1,26 @@
 'use strict';
-const express = require('express');
+const pj_scraper = require('../../index.js');
+const fs = require('fs');
+const util = require('util');
+
 const puppeteer = require('puppeteer');
 const { createLogger, transports } = require('winston');
-const http = require('http');
-const https = require('https');
 const assert = require('assert');
 const path = require('path');
-const keyCert = require('key-cert');
-const Promise = require('bluebird');
-const Proxy = require('http-mitm-proxy');
 
 const debug = require('debug')('pj-scraper:test');
 const { BingDesktopScraper } = require('../../src/modules/bing-desktop');
 
-const httpPort = 3012;
-const httpsPort = httpPort + 1;
-const proxyPort = httpPort + 2;
-
-const fakeSearchEngine = express();
-fakeSearchEngine.get('/search', (req, res) => {
-    debug('q=%s', req.query.q);
-    const pageNumber = ((req.query.start/10) || 0)  + 1;
-    res.sendFile(path.join(__dirname, '../mocks/bing-desktop/' + req.query.q + '_page' + pageNumber + '.html'));
-});
-fakeSearchEngine.use(express.static('test/mocks/bing-desktop', {extensions: ['html']}));
-
 describe('Module Bing Desktop', function(){
 
-    let httpServer, httpsServer, proxy;
     before(async function(){
         // Here mount our fake engine in both http and https listen server
-        httpServer = http.createServer(fakeSearchEngine);
-        httpsServer = https.createServer(await keyCert(), fakeSearchEngine);
         
-        proxy = Proxy();
-        proxy.onRequest((ctx, callback) => {
-            ctx.proxyToServerRequestOptions.host = 'localhost';
-            ctx.proxyToServerRequestOptions.port = (ctx.isSSL) ? httpsPort : httpPort;
-            ctx.proxyToServerRequestOptions.headers['X-Forwarded-Host'] = 'ProxiedThroughFakeEngine';
-            debug('connection proxied askedHost=%s toPort=%s', ctx.clientToProxyRequest.headers.host, ctx.proxyToServerRequestOptions.port);
-            return callback();
-        });
-
-        await Promise.promisify(proxy.listen, { context: proxy })({ port: proxyPort });
-        await Promise.promisify(httpServer.listen, {context: httpServer})(httpPort);
-        await Promise.promisify(httpsServer.listen, {context: httpsServer})(httpsPort);
         debug('Fake http search engine servers started');
     });
 
     after(function(){
-        proxy.close();
-        httpsServer.close();
-        httpServer.close();
+        
     });
 
     let browser;
@@ -59,10 +28,21 @@ describe('Module Bing Desktop', function(){
     beforeEach(async function(){
         debug('Start a new browser');
         browser = await puppeteer.launch({
-            //dumpio: true,
             //headless: false,
             ignoreHTTPSErrors: true,
-            args: [ '--proxy-server=http://localhost:' + proxyPort ]
+            //userDataDir: './tmp',
+            args: [
+                '--disable-sync',
+                '--start-maximized',
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-infobars',
+                '--window-position=0,0',
+                '--ignore-certifcate-errors',
+                '--ignore-certifcate-errors-spki-list',
+            ]
+            //dumpio: true,
+            //headless: false,
         });
         debug('Open a fresh page');
         page = await browser.newPage();
@@ -84,17 +64,22 @@ describe('Module Bing Desktop', function(){
         const bingDesktopScraper = new BingDesktopScraper({
             config: {
                 search_engine_name: 'bing_desktop',
-                throw_on_detection: true,
-                keywords: ['shein'],
+                //throw_on_detection: true,
+                keywords: ['cloud service'],
                 logger: testLogger,
                 scrape_from_file: '',
+                scrape_from_string: fs.readFileSync('../mocks/bing_desktop.html', 'utf8'),
             }
         });
+
         bingDesktopScraper.STANDARD_TIMEOUT = 500;
+
         return bingDesktopScraper.run({page}).then(({results, metadata, num_requests}) => {
-            assert.strictEqual(num_requests, 1, 'Must do one request');
-            assert.strictEqual(results['shein']['1'].results.length, 10, 'Must have 10 organic results parsed');
+            //console.dir(results, {depth: null, colors: false});
+            assert(results['cloud service']['1']['top_ads'].length >= 1);
+            //assert(results['cloud service']['1']['bottom_ads'].length > 1);
         });
+
     });
 
 });
